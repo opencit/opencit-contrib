4e838db1b868ed4e7c15950f6bd31abb6d4601f3
Ryan Savino <ryan.k.savino@intel.com>
2016-08-09 20:04:06 -0700

    v2.1.0+next-patch: removed patch file; it is now generated corrected klocwork.sh moved permission setting in cit-patched-tpm-tools/pom.xml

--- klocwork.sh
+++ klocwork.sh
@@ -10,13 +10,10 @@ initialize() {
 }
 
 generateBuildSpecs() {
-  if [ ! -d "cit-patched-tpm-tools/target" ]; then
-    (cd cit-patched-tpm-tools && mvn install)
-  fi
-  
-  (cd cit-patched-tpm-tools/target/patched-tpm-tools-2.1-SNAPSHOT/tpm-tools-1.3.8-patched && LDFLAGS="-L/usr/local/lib" ./configure --prefix=/usr/local)
-  (cd cit-patched-tpm-tools/target/patched-tpm-tools-2.1-SNAPSHOT/tpm-tools-1.3.8-patched && make clean)
-  (cd cit-patched-tpm-tools/target/patched-tpm-tools-2.1-SNAPSHOT/tpm-tools-1.3.8-patched && kwinject --output "${buildSpecsDirectory}/tpm_tools_patched.out" make)
+  (cd cit-patched-tpm-tools && mvn install)
+  (cd cit-patched-tpm-tools/target/tpm-tools-1.3.8-patched && LDFLAGS="-L/usr/local/lib" ./configure --prefix=/usr/local)
+  (cd cit-patched-tpm-tools/target/tpm-tools-1.3.8-patched && make clean)
+  (cd cit-patched-tpm-tools/target/tpm-tools-1.3.8-patched && kwinject --output "${buildSpecsDirectory}/tpm_tools_patched.out" make)
 }
 
 buildProject() {
