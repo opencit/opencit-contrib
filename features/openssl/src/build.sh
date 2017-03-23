e5cf55e3651bbb78a8448f3e108e4ba444cf839b
purvades <purva.deshpande@intel.com>
2017-03-23 16:32:11 -0700

    v1.0+cit-3.1-rc1+next+SDL-compile-with-defenses-enabled: Adding flag CFLAGS=”-fstack-protector-strong” instead of CFLAGS="-fstack-protector" as GCC version used is 4.9 or above
    Change-Id: I43b715dce6e240f78b2f03de2137a994d7b63eb0

--- features/openssl/src/build.sh
+++ features/openssl/src/build.sh
@@ -4,7 +4,7 @@ OPENSSL="openssl-${OPENSSL_VERSION}"
 
 export PREFIX="${PREFIX:-/opt/mtwilson/share/openssl}"
 export LINUX_TARGET="${LINUX_TARGET:-generic}"
-export CFLAGS="-fstack-protector -fPIE -fPIC -O2 -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security"
+export CFLAGS="-fstack-protector-strong -fPIE -fPIC -O2 -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security"
 export LDFLAGS="-z noexecstack -z relro -z now -pie"
 
 
