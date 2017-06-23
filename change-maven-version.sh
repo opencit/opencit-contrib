d631cdb5134fb9210c49fc260aa00549948d3c1d
Ryan Savino <ryan.k.savino@intel.com>
2017-06-23 00:16:08 -0700

    v1.0+cit-3.2+next-patch+release-cit-3.2.1: adjusted poms and change-maven-version.sh to correctly change all maven versions

--- change-maven-version.sh
+++ change-maven-version.sh
@@ -33,3 +33,13 @@ ant ready
 if [ $? -ne 0 ]; then echo "Failed to maven install \"maven\" projects" >&2; exit 3; fi
 $changeVersionCommand
 if [ $? -ne 0 ]; then echo "Failed to change maven version at top level" >&2; exit 3; fi
+$changeParentVersionCommand
+if [ $? -ne 0 ]; then echo "Failed to change maven parent versions" >&2; exit 3; fi
+ant ready
+if [ $? -ne 0 ]; then echo "Failed to maven install \"maven\" projects, 2nd time" >&2; exit 3; fi
+(cd prebuild && $changeVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven version on \"prebuild\" folder" >&2; exit 3; fi
+(cd packages && $changeVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven version on \"packages\" folder" >&2; exit 3; fi
+(cd packages && $changeParentVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven parent versions in \"packages\" folder" >&2; exit 3; fi
